0cf5bf9298318798c762e89c9b4b70e8ffead6ad
Andreas Auernhammer <aead@mail.de>
2019-10-29 00:19:51 +0100

    sioutil: add `NativeAES` function
    This commit adds the `NativeAES()` utility function.
    
    As a user of the `sio` package you have to pick an
    AEAD algorithm. Usually, AES-GCM gives better performance
    if the hardware platform provides AES-specific CPU
    instructions and an asm implementation is available.
    
    However, if one of these two conditions is not true,
    then AES-GCM may be slower than other AEAD algorithms
    (e.g. ChaCha20-Poly1305). Furthermore, generic AES-GCM
    is not a constant-time implementation.
    See: https://golang.org/pkg/crypto/aes
    
    Therefore, users of `sio` should check whether an asm
    implementation is available and whether the CPU provides
    AES instructions:
    ```
    if sioutil.NativeAES() {
      // eventually use AES-GCM
    } else {
      // eventually use ChaCha20-Poly1305
    }
    ```

--- sioutil/sio.go
+++ sioutil/sio.go
@@ -7,6 +7,8 @@ package sioutil
 
 import (
 	"io"
+
+	"golang.org/x/sys/cpu"
 )
 
 type nopCloser struct {
@@ -15,8 +17,35 @@ type nopCloser struct {
 
 func (nopCloser) Close() error { return nil }
 
-// NopCloser returns a WriteCloser with a no-op Close method
-// wrapping the provided Writer w.
+// NopCloser returns a WriteCloser that wraps w
+// and implements Close as a no-op.
 func NopCloser(w io.Writer) io.WriteCloser {
 	return nopCloser{w}
+}
+
+// NativeAES returns true when the executing CPU
+// provides AES-GCM hardware instructions and
+// an optimized assembler implementation is
+// available.
+//
+// It is strongly recommended to only use AES-GCM
+// when NativeAES() returns true. Otherwise, the
+// AES-GCM implementation may be vulnerable to
+// timing attacks.
+// See: https://golang.org/pkg/crypto/aes
+func NativeAES() bool {
+	if cpu.X86.HasAES && cpu.X86.HasPCLMULQDQ {
+		return true
+	}
+	if cpu.ARM64.HasAES {
+		return true
+	}
+
+	// On s390x, aes.NewCipher(...) returns a type
+	// that provides AES asm implementations only
+	// if all (CBC, CTR and GCM) AES hardware
+	// instructions are available.
+	// See: https://golang.org/src/crypto/aes/cipher_s390x.go#L39
+	return cpu.S390X.HasAES && cpu.S390X.HasAESCBC && cpu.S390X.HasAESCTR &&
+		(cpu.S390X.HasAESGCM || cpu.S390X.HasGHASH)
 }
